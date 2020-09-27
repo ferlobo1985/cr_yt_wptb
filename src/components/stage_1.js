21714a35c9c3fa4b66d15804a6e152915b6f747f
devdummy <dev.teach.user.dummy@gmail.com>
2020-09-27 20:22:31 -0300

    stage one

--- src/components/stage_1.js
+++ src/components/stage_1.js
@@ -1,10 +1,43 @@
 import React from 'react';
-
+import { Form,Button,Alert} from  'react-bootstrap';
+import { useFormik } from  'formik';
+import * as Yup from 'yup';
 
 const Stage1 = () => {
+
+    const formik = useFormik({
+        initialValues:{ player:'' },
+        validationSchema: Yup.object({
+            player: Yup.string().max(5,'Must be 15 character or less')
+            .required('Sorry, the name is required')
+        }),
+        onSubmit: ( values)=>{
+            console.log(values)
+        }
+    })
+
     return(
         <>
-    stage 1
+            <Form onSubmit={formik.handleSubmit} className="mt-4">
+                <Form.Group>
+                    <Form.Control
+                        type="text"
+                        placeholder="Add player name"
+                        name="player"
+                        onChange={formik.handleChange}
+                        value={formik.values.player}
+                    />
+                </Form.Group>
+
+                { formik.errors.player && formik.touched.player ?
+                    <Alert variant="danger">
+                        { formik.errors.player}
+                    </Alert>
+                :null}
+                <Button className="miami" variant="primary" type="submit">
+                    Add player
+                </Button>
+            </Form>
         </>
     )
 }
